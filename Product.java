1bbe3ae1f8f63b2626aee4a25f9d5d61a07fbeec
eatjac <eatjac@gmail.com>
2019-04-08 21:53:08 -0500

    Updated comments; fixed formatting

--- Product.java
+++ Product.java
@@ -1,22 +1,34 @@
-public class Product {
+// This class contains the object description of what is contained in the ShoppingCart
+
+public class Product 
+{
+	// Private attributes
 	private String title;
 	private double price;
 	
-	public Product (String t, double p) {
+	// Constructor
+	public Product (String t, double p) 
+	{
 		this.title = t;
 		this.price = p;
 	}
 	
-	public String getTitle() {
+	// Accessor methods
+	public String getTitle() 
+	{
 		return title;
 	}
 	
-	public double getPrice() {
+	public double getPrice() 
+	{
 		return price;
 	}
 	
-	public boolean equals(Object o) {
-		if (o instanceof Product) {
+	// Comparator methods
+	public boolean equals(Object o) 
+	{
+		if (o instanceof Product) 
+		{
 			Product p = (Product)o;
 			return p.getTitle().equals(title);
 		}
--- ShoppingCart.java
+++ ShoppingCart.java
@@ -1,37 +1,51 @@
+// This class is based on being a container for Product instances
+
 import java.util.*;
 
 public class ShoppingCart {
 	
-	private ArrayList items;
+	// Private attributes
+	private ArrayList<Product> items;
 	
-	public ShoppingCart() {
-		items = new ArrayList();
+	
+	// Default constructor
+	public ShoppingCart() 
+	{
+		items = new ArrayList<Product>();
 	}
 	
-	public double getBalance() {
+	
+	// Accessor methods
+	public double getBalance() 
+	{
 		double balance = 0.00;
-		for (Iterator i = items.iterator(); i.hasNext();){
+		for (Iterator<Product> i = items.iterator(); i.hasNext();){
 			Product item = (Product)i.next();
 			balance += item.getPrice();
 		}
 		return balance;
 	}
 	
-	public void addItem(Product item) {
+	public int getItemCount() 
+	{
+		return items.size();
+	}
+	
+	
+	// Utility methods
+	public void addItem(Product item) 
+	{
 		items.add(item);
 	}
 	
-	public void removeItem(Product item)
-			throws ProductNotFoundException {
+	public void removeItem(Product item) throws ProductNotFoundException 
+	{
 		if (!items.remove(item)) {
 			throw new ProductNotFoundException();}
 	}
 	
-	public int getItemCount() {
-		return items.size();
-	}
-	
-	public void empty() {
+	public void empty() 
+	{
 		items.clear();
 	}
 }
